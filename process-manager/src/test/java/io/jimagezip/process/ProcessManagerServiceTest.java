c871cb07927b2d8ede030a5f6521ce72c07492f4
James Strachan <james.strachan@gmail.com>
2014-10-20 15:36:43 +0100

    first spike of fabric8's V1s ProcessManager migrated to be usable as a process manager for jimagezip

---
+++ process-manager/src/test/java/io/jimagezip/process/ProcessManagerServiceTest.java
@@ -0,0 +1,86 @@
+/**
+ *  Copyright 2005-2014 Red Hat, Inc.
+ *
+ *  Red Hat licenses this file to you under the Apache License, version
+ *  2.0 (the "License"); you may not use this file except in compliance
+ *  with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ *  implied.  See the License for the specific language governing
+ *  permissions and limitations under the License.
+ */
+package io.jimagezip.process;
+
+import com.google.common.io.Files;
+import io.jimagezip.process.InstallOptions;
+import io.jimagezip.process.InstallTask;
+import io.jimagezip.process.service.ProcessManagerService;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import javax.management.MalformedObjectNameException;
+import java.io.File;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.nio.charset.Charset;
+
+import static java.lang.String.format;
+import static java.util.UUID.randomUUID;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertNotNull;
+
+public class ProcessManagerServiceTest {
+
+    protected File installDir;
+    protected ProcessManagerService processManagerService;
+    InstallOptions installOptions;
+    InstallTask postInstall;
+    String firstJvmOption = "-Dfoo=bar";
+    String secondJvmOption = "-server";
+
+    @Before
+    public void setUp() throws Exception {
+        String basedir = System.getProperty("basedir", ".");
+        installDir = new File(basedir + "/target/processes/" + getClass().getName()).getCanonicalFile();
+        System.out.println("Installing processes to " + installDir.getAbsolutePath());
+
+        processManagerService = new ProcessManagerService(installDir);
+        processManagerService.init();
+
+        installOptions = new InstallOptions.InstallOptionsBuilder().
+                jvmOptions(firstJvmOption, secondJvmOption).
+                url("mvn:io.jimagezip.images.examples/cxf-cdi/2.0.0-SNAPSHOT/zip/image").build();
+    }
+
+    @After
+    public void destroy() throws Exception {
+    }
+    @Test
+    public void shouldGenerateJvmConfig() throws Exception {
+        Installation installation = processManagerService.install(installOptions, postInstall);
+        String generatedEnvSh = Files.toString(new File(installDir, "1/env.sh"), Charset.forName("UTF-8"));
+
+        System.out.println("Found env.sh: " + generatedEnvSh);
+
+        ProcessController controller = installation.getController();
+        assertNotNull("controller", controller);
+        controller.start();
+
+        System.out.println("Started process!");
+
+        Thread.sleep(1000);
+
+        Long pid = controller.getPid();
+        assertNotEquals("Should have a PID!", pid);
+
+        controller.stop();
+    }
+
+}
