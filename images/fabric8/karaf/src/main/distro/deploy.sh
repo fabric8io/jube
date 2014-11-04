7c4a4cf100de4fc7c97125ea3f73c81c1b604d79
Jimmi Dyson <jimmidyson@gmail.com>
2014-11-04 12:25:36 +0000

    Fix up deploy script

--- images/fabric8/karaf/src/main/distro/deploy.sh
+++ images/fabric8/karaf/src/main/distro/deploy.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-if [ -d "$APP_DIR/repository" ]; then
+if [ -d "$APP_BASE/maven/repository" ]; then
   echo "Copying offline repo"
-  cp -rf $APP_DIR/repository/* $APP_DIR/system/
+  cp -rf $APP_BASE/maven/repository/* $APP_BASE/system/
 fi
 
-if [ -d "$APP_DIR/kars" ]; then
-  ln -s $APP_DIR/kars/* $i $APP_DIR/deploy/
+if [ -d "$APP_BASE/maven/kars" ]; then
+  ln -s $APP_BASE/maven/kars/* $i $APP_BASE/deploy/
 fi
