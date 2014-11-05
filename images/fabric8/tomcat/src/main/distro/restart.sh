cc826d4497ea2356828595a5f60f4f287d95b533
Claus Ibsen <claus.ibsen@gmail.com>
2014-11-05 15:15:28 +0100

    #1: First cut of tomcat jube image

---
+++ images/fabric8/tomcat/src/main/distro/restart.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+#
+#  Copyright 2005-2014 Red Hat, Inc.
+#
+#  Red Hat licenses this file to you under the Apache License, version
+#  2.0 (the "License"); you may not use this file except in compliance
+#  with the License.  You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+#  implied.  See the License for the specific language governing
+#  permissions and limitations under the License.
+#
+
+#
+# Discover the APP_BASE from the location of this script.
+#
+if [ -z "$APP_BASE" ] ; then
+  DIRNAME=`dirname "$0"`
+  APP_BASE=`cd "$DIRNAME"; pwd`
+  export APP_BASE
+fi
+
+source ${APP_BASE}/env.sh
+
+$APP_BASE/stop
+$APP_BASE/start
+
